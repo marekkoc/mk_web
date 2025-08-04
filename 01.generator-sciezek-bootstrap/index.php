cf8854a0afbd6c9c9469a71b90ec1010e5c629c2
marekkoc <m2019k1020@gmail.com>
2025-08-04 09:31:27 +0200

    Kurs: ustawinie serwera...

---
+++ 01.generator-sciezek-bootstrap/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="pl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=devide-width, initial-scale=1.0>
+  <title>Path Generator</title>
+
+  <!-- Bootstrap CSS CDN --->
+  <link href="https://https://cdn.jsdelivr.net/bootstrap@5.3.0/dist/scc/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+ <!-- Bootstrap Container -->
+ <div class="container mt-5"
+
+    <!-- Header -->
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center mb-4">Paht generator to files!"</h1>
+        <p class="lead text-center text-muted">
+          A tool for folder and files names into full system paths.
+        </p>
+      </div>
+    </div>
+
+<!-- Test bootstrap - przycisk testowy --->
+<div class="row mt-4">
+  <div class="col-12 text-center">
+    <button type="button" class="btn btn-primary">
+      Test Bootstrap - if it is blue that Bootstrap works!
+    </button>
+
+  </div>
+</div>
+
+<!--- Miejsce na przyszły formularz --->
+<div class="row mt-5">
+  <div class="col-12">
+    <!-- Tu będzie formularz! --->
+    <div class="alert alert-info" role="alert">
+      <strong>Miejsce na formularz: </strong> Tutaj w następnym kroku dodamy formularz do generowania ścierzek!
+    </div>
+  </div>
+</div>
+
+
+
+  <!-- Bootstrap JavaScript CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
